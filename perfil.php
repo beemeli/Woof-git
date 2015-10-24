696ffae8d2fdb7179381b0568226b12b85371c1b
Melissa Soto <beemeli15@gmail.com>
2015-10-24 14:09:16 -0500

    algo
    algo

--- perfil.php
+++ perfil.php
@@ -16,7 +16,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Registrar usuario</title>
+        <title>Ver Perfil</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
@@ -31,7 +31,7 @@ and open the template in the editor.
     <body>
         <div>
             
-
+            
             <div id="divRespuesta"></div>
 
         
