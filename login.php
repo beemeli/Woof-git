2d7f63c49f49f8bce6279ff3ae9782515e0f9d2a
cuchus <cuchus@gmail.com>
2015-10-24 17:27:35 -0500

    v 1.1
    Login versiòn 1

--- login.php
+++ login.php
@@ -6,26 +6,39 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Login</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <title>Woof Login</title>
+        <meta charset="utf-8">
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        
+        
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/ct-navbar.css" type="text/css">
+        <script src="js/ct-navbar.js"></script>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script type="text/javascript" src="js/controller.js"></script>
         
         
     <body>
-        <div>
-            
-            <form action="Recursos/Servicios/iniciarSesion.php" method="post">
-                <input type="text" placeholder="usuario" id="usuario" name="usuario"/>
-                <input type="password" placeholder="contraseña" id="password" name="password"/>
-                <br/>
-                <input type="submit" value="Iniciar Sesión"/>
-                
-            </form>
-            
-            <div id="divRespuesta"></div>
-            
-            
-        </div>
+        
+        
+            <div class="container">
+
+      <form  action="Recursos/Servicios/iniciarSesion.php" method="post">
+        <h2 >Identifícate</h2>
+        <label for="inputEmail" class="sr-only">Usuario</label>
+        <input input type="text" placeholder="usuario" id="usuario" name="usuario" class="form-control" required autofocus>
+        <label for="inputPassword" class="sr-only">Contraseña</label>
+        <input type="password" placeholder="contraseña" id="password" name="password" class="form-control" required>
+        <br>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" value="Iniciar Sesión">Entrar</button>
+      </form>
+
+    </div> 
+        
+    
     </body>
 </html>
