f1c41ff7ba451a3cfc2ab6e2655da526f80f2c86
cuchus <cuchus@gmail.com>
2015-10-27 06:39:30 -0600

    v1.1
    modificar perfil

--- modificarPerfil.php
+++ modificarPerfil.php
@@ -8,193 +8,179 @@
         header('Location: login.php');
     }
 ?>
-    <!DOCTYPE html>
-    <!--
+<!DOCTYPE html>
+<!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-    <html>
-
+<html>
     <head>
         <title>Modificar Perfil</title>
         <meta charset="UTF-8">
-        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+         <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-        <script src="js/ct-navbar.js"></script>
-        <link rel="stylesheet" href="css/style.css" type="text/css">
+    <script src="js/ct-navbar.js"></script>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script type="text/javascript" src="js/controller.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    
 
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
 
-        <!--Import Google Icon Font-->
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <!--Import jQuery before materialize.js-->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
-        <link rel="stylesheet" href="css/ct-navbar.css" type="text/css">
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <link rel="stylesheet" href="css/ct-navbar.css" type="text/css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>        
         <script type="text/javascript" src="js/controllerModPerfil.js"></script>
         <script>
-            var test = '<?php echo $_SESSION['
-            usuarioSesion '] ?>';
-            console.log("user-->" + test);
+            var test = '<?php echo $_SESSION['usuarioSesion'] ?>';
+            console.log("user-->"+test);
         </script>
 
-
-
     </head>
-
     <body>
-        <div id="navbar-full">
-            <div id="navbar">
-                <!--    
+        
+        
+    <div id="navbar-full">
+        <div id="navbar">
+            <!--    
         navbar-default can be changed with navbar-ct-blue navbar-ct-azzure navbar-ct-red navbar-ct-green navbar-ct-orange  
         -->
-                <nav class="navbar navbar-ct-blue navbar-fixed-top navbar-transparent" role="navigation">
+            <nav class="navbar navbar-ct-blue navbar-fixed-top navbar-transparent" role="navigation">
 
-                    <div class="container">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
 
-                            <a class="navbar-brand navbar-brand-logo" href="">
-                                <div class="logo">
-                                    <img src="images/wooficonNuevo.png">
-                                </div>
-                                <div class="brand"> Woof a Friend </div>
-                            </a>
-                        </div>
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li>
-                                    <a href="#">
-
-                                        <p>Adopta</p>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#">
-                                        <p>Adopta mas</p>
-                                    </a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-
-                                        <p>Usuario <b class="caret"></b></p>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="login.php">Iniciar sesión</a></li>
-                                        <li><a href="registrarUsuario.php">Regístrate</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="perfil.php">Ver Perfil</a></li>
-                                        <li><a href="modificarPerfil.php">Modificar Perfil</a></li>
-
-                                    </ul>
-                                </li>
-                            </ul>
-
-
-                        </div>
-                        <!-- /.navbar-collapse -->
+                        <a class="navbar-brand navbar-brand-logo" href="index.php">
+                            <div class="logo">
+                                <img src="images/wooficonNuevo.png">
+                            </div>
+                            <div class="brand"> Woof a Friend </div>
+                        </a>
                     </div>
-                    <!-- /.container-fluid -->
-                </nav>
-                <div class="blurred-container">
-                    <div class="img-src" style="background-image: url('images/bg.png')"></div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="#">
+
+                                    <p>Adopta</p>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <p>Adopta mas</p>
+                                </a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+                                    <p>Usuario <b class="caret"></b></p>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="login.php">Iniciar sesión</a></li>
+                                    <li><a href="registrarUsuario.php">Regístrate</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="perfil.php">Ver Perfil</a></li>
+                                    <li><a href="modificarPerfil.php">Modificar Perfil</a></li>
+
+                                </ul>
+                            </li>
+                        </ul>
+
+
+                    </div>
+                    <!-- /.navbar-collapse -->
                 </div>
+                <!-- /.container-fluid -->
+            </nav>
+            <div class="blurred-container">
+                <div class="img-src" style="background-image: url('images/bg.png')"></div>
             </div>
-            <!--  end navbar -->
-
         </div>
-        <!-- end menu-dropdown -->
-        <div class="main">
-            <div class="container">
-                
+        <!--  end navbar -->
+
+    </div>
+    <!-- end menu-dropdown -->
+
+
+
+
+
+
+    <div class="main">
+        <div class="container">
+
+
+            <form>
+                <div id="divRespuesta"></div>
+
                 <h1 class="text-center">Modificar Perfil </h1>
+                <br>
+                <br>
 
+                <div class="input-field col s12 ">
+                    <input id="password" name="password" type="password" class="validate" required>
+                    <label for="password">Contraseña actual</label>
+                </div>
 
-                <form>
+                <div class="input-field col s12">
+                    <input id="password1" name="password1" type="password" class="validate" required>
+                    <label for="password1">Nueva Contraseña</label>
+                </div>
 
-                    <div class="input-field col s12 ">
-                        <input id="password" name="password" type="password" class="validate" required>
-                        <label for="password">Contraseña actual</label>
-                    </div>
+                <div class="input-field col s12 ">
+                    <input id="password2" name="password2" type="password" class="validate" required>
+                    <label for="password2">Nueva Contraseña</label>
+                </div>
 
-                    <div class="input-field col s12">
-                        <input id="password1" name="password1" type="password" class="validate" required>
-                        <label for="password1">Nueva Contraseña</label>
-                    </div>
-
-                    <div class="input-field col s12">
-                        <input id="password2" name="password2" type="password" class="validate" required>
-                        <label for="password2">Nueva Contraseña</label>
-                    </div>
+                <div class="input-field col s12 ">
+                    <!--<label for="correo">Correo Electronico</label>-->
+                    <input id="correo" name="correo" type="email" class="validate" required>
                     
-                      <div class="input-field col s12 ">
-                        <input id="correo" name="correo" type="email" class="validate" required>
-                        <label for="correo">Correo</label>
-                    </div>
-                    
-                   
+                </div>
 
-                </form>
-               
-            
-            <button id="modificarPerfil" class="btn waves-effect waves-light  blue lighten-3 botonEnviar">Enviar
+
+                <br>
+            </form>
+
+            <button class="btn waves-effect waves-light  blue lighten-3 botonEnviar">Enviar
                 <i class="material-icons right">send</i>
             </button>
         
-                 <div id="divRespuesta"></div>
-
-            </div>
-
-        </div>
-
-<!--antiguo codigo -->
+            
+           
         
-        <form>
-        <input type="password" id="password" placeholder="Contraseña actual" />
-        <br/>
-        <br/>
-        <input type="password" id="password1" placeholder="Nueva Contraseña" />
-        <br/>
-        <br/>
-        <input type="password" id="password2" placeholder="Nueva Contraseña" />
-        <br/>
-        <br/>
-        <input type="tetx" id="correo" placeholder="Correo" />
-        <br/>
-        <br/>
-        </form>
-
-        <div id="divRespuesta"></div>
-        <div id="modificarPerfil" class="botonEnviar">
-            Aceptar
+        
+        
+            
+                       
+        </div>  
         </div>
-
-       
+        <br>
+        <br>
     </body>
-
-    </html>
+</html>
