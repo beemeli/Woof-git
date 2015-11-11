eafbfd2cdba8b5ba59225b273108875c21e07c8b
Melissa Soto <beemeli15@gmail.com>
2015-11-11 10:55:38 -0600

    algo
    algo

--- registarCentro.php
+++ registarCentro.php
@@ -143,11 +143,29 @@
                     <input id="telefono" name="telefono" type="text" class="validate" maxlength="15" required>
                     <label for="telefono">Teléfono</label>
                 </div>
-                
+                <!--
                 <div class="input-field col s12">
                     <input id="direccion" name="direccion" type="text" class="validate" maxlength="80" required>
                     <label for="direccion">Dirección</label>
                 </div>
+                -->
+                <div class="input-field col s12">
+                    <input id="calle" name="calle" type="text" class="validate" maxlength="80" required>
+                    <label for="calle">Calle</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="numero" name="numero" type="text" class="validate" maxlength="80" required>
+                    <label for="numero">Número</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="colonia" name="colonia" type="text" class="validate" maxlength="80" required>
+                    <label for="colonia">Colonia</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="cp" name="cp" type="text" class="validate" maxlength="80" required>
+                    <label for="cp">C.P.</label>
+                </div>
+                <div id="verMapaBoton">Ver mapa</div>
                 <div id="googleMap1" style="width:250px;height:190px;"></div>
 
                 
