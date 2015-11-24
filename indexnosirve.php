97dcf3d626ef06612582239882ccf7e23eb5b2d8
cuchus <cuchus@gmail.com>
2015-11-24 12:36:18 -0600

    cambios
    cambios

--- indexnosirve.php
+++ indexnosirve.php
@@ -1,41 +1,48 @@
 <html>
 
 <head>
-        <title>Generar Reportes</title>
-        <meta charset="utf-8">
-        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <title>Woof</title>
+    <meta charset="utf-8">
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+    <script src="js/ct-navbar.js"></script>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/controller.js"></script>
 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         
         <script src="js/ct-navbar.js"></script>
         <link rel="stylesheet" href="css/style.css" type="text/css">
-
+    
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script type="text/javascript" src="js/controller.js"></script>
 
 
-        <!--Import Google Icon Font-->
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
 
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
 
-        <!--Import jQuery before materialize.js-->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
-        <link rel="stylesheet" href="css/ct-navbar.css" type="text/css">    
-        <script type="text/javascript" src="js/controllerGenerarReporte.js"></script>
-        <script>
-            var test = '<?php echo $_SESSION['usuarioSesion'] ?>';
-            console.log("user-->"+test);
-        </script>
-        
-    </head>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <link rel="stylesheet" href="css/ct-navbar.css" type="text/css">
+
+
+</head>
+
 
 
 
@@ -50,7 +57,7 @@
         
     
 ?><!-- Sin registrar -->
-        <div id="navbar-full">
+         <div id="navbar-full">
             <div id="navbar">
                 <!--    
         navbar-default can be changed with navbar-ct-blue navbar-ct-azzure navbar-ct-red navbar-ct-green navbar-ct-orange  
@@ -80,13 +87,13 @@
                                 <li>
                                     <a href="consultarCentro.php">
 
-                                        <p>Centros de Adopción</p>
+                                        <p>Centros de adopción</p>
                                     </a>
                                 </li>
 
                                 <li>
-                                    <a href="consultarAcopio.php">
-                                        <p>Centros de Acopio</p>
+                                    <a href="#">
+                                        <p>Adopta mas</p>
                                     </a>
                                 </li>
                                 <li class="dropdown">
@@ -98,7 +105,8 @@
                                         <li><a href="login.php">Iniciar sesión</a></li>
                                         <li><a href="registrarUsuario.php">Regístrate</a></li>
                                         <li class="divider"></li>
-
+                                        <li><a href="perfil.php">Ver Perfil</a></li>
+                                        <li><a href="modificarPerfil.php">Modificar Perfil</a></li>
 
                                     </ul>
                                 </li>
@@ -332,6 +340,75 @@ if(isset($_SESSION['usuarioSesion'])){
 } 
 ?>
 
+        
+ <!-- end menu -->
+        
+        
+        
+        
+        <div class="main">
+
+
+            <div class="container" id="contenido">
+                Usuario no registrado: <br/>
+                <a href="registrarUsuario.php">Registrar Usuario</a><br/>
+                <br/>
+                Admin, usuario, usuario NO registrado:<br/>
+                <a href="consultarAcopio.php">Consultar Centros de Acopio</a><br/>
+                <a href="consultarCentro.php">Consultar Centros</a><br/>
+                <a href="inicio.php">Ver perritos por tamaño **** (ver las imágenes en un carousel o algo así cool y que cuando  haya hover, aparezca encima de la imagen la info del perrito (o algo así))</a><br
+                <br/>
+                Admin y usuario <br/>
+                <a href="recursos/Servicios/cerrarSesion.php">Cerrar Sesión</a><br/>
+                <br/>
+                Usuario: <br/>
+                <a href="perfil.php">Perfil</a><br/>
+                <a href="registrarSolicitudAdopcion.php">Solicitar Adopción </a><br/>
+                <a href="consultarSolicitudAdopcionUsuario.php">Consultar solicitud usuario</a><br/>
+                
+                <br/>
+                Admin:<br/>
+                <a href="consultarSolicitudAdopcion.php">Consultar Solicitud Adopcion</a><br/>
+                <a href="registarCentro.php">Registrar Centro</a><br/>
+                <a href="registrarAcopio.php">Registrar Centro de Acopio</a><br/>
+                <a href="registrarPerrito.php">Registrar Perrito</a><br/>
+                <a href="GenerarReportes.php">Generar Reportes</a><br/>
+                <a href="MascotaVirtual.php">Mascota Virtual</a><br/>
+                <label id="mv">Mascota Virtual</label><br/>
+                <a href="recursos/Servicios/cerrarSesion.php">Cerrar Sesión</a><br/>
+
+                
+                
+                
+
+
+                <h1> Woof a Friend </h1>
+
+                <p>Wooof a friend es la mejor opción para adoptar perritos en el Distrito Federal. Así mismo, es una red social en donde podrás compartir información de perritos que te encuentres en las calles de la ciudad, para que una persona de los Centros de Apoyo acuda y lo ayude. Únete y sé parte del cambio social de México.</p>
+
+
+                <div class="row">
+                    <!-- inicio tabla-->
+                    <div class="col s12 m6 l4">
+                        <h4 class="light">Propósito</h4>
+                        <div class="card medium">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="images/noti.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">Educación<i class="material-icons right">more_vert</i></span>
+                                <p>
+                                    Conciencia, acción y cambio.
+                                </p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Educación<i class="material-icons right">close</i></span>
+                                <p><br>Buscamos crear conciencia en la población mexicana, fomentando el cuidado y trato digno de las mascotas de México.</p>
+                            </div>
+                        </div>
+                    </div>
+
+
                     <!-- end menu -->
 
 
@@ -466,7 +543,31 @@ if(isset($_SESSION['usuarioSesion'])){
                                 </div>
 
                             </div>
- 
+                            <!-- fin tabla -->
+
+                            <!-- inicio anterior index -->
+
+                            <!--          <div style="position:absolute;top:150px;background-color: #FF9966; height:20px;width:500px;">
+>>>>>>> origin/master:index.html
+            Tamagotchi
+        </div>
+        <div id="contenido" style="position:absolute;top:180px;background-color: #FFFFCC; height:200px;width:500px;">
+            <img src="images/perroAlimento.png" alt="perro" style="height:175px"/>
+        </div>
+
+        
+        <div  style="position:absolute;top:390px;background-color: #FF9966; height:20px;width:500px;">
+            <div id="alimentar" class="accionMascota" style="float:left; padding-right:20px; ">Alimentar </div>
+            <div id="jugar" class="accionMascota" style="float:left; padding-right:20px;">Jugar</div>
+            <div id="higiene" class="accionMascota" style="float:left; padding-right:20px;">Higiene</div>
+            <div id="salud" class="accionMascota" style="float:left; padding-right:20px;">Salud</div>
+        </div>
+        
+        <div id="divResultado" style="position:absolute;top:420px;background-color: #FFFF99; height:20px;width:500px;">
+            
+        </div>
+        
+       -->
 
                         </div>
 
@@ -482,7 +583,3 @@ if(isset($_SESSION['usuarioSesion'])){
 
 
 </html>
-
-
-
-
